ce2b4374bc5bb34790a540cd628e387394209e7c
Jainvidhi <95088131+Jainvidhi@users.noreply.github.com>
2022-03-31 20:51:34 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,13 @@
 from flask import*
 
 @app.route("/")
+import pyttsx3
+text_speech = pyttsx3.int()
+result = input("text:")
+text_speech.say(result)
+text_speech.runAndWait()
+
+
 def hello_world():
    return render_template('index.html') 
 

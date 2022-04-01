cc11346b073f94602d757bf83031158d356e4e17
ChiragRohada <91385961+ChiragRohada@users.noreply.github.com>
2022-04-01 15:27:58 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,19 @@
 from flask import*
 
 @app.route("/")
+def hello_world():
+   return render_template('index.html') 
+
+@app.route("/tts")
 import pyttsx3
 text_speech = pyttsx3.int()
 result = input("text:")
 text_speech.say(result)
 text_speech.runAndWait()
 
-
 def hello_world():
    return render_template('index.html') 
+
 
 
 if __name__ == '__main__':  

88d6498ea1c443c8808faa52dc7d0d36c2da85c4
ChiragRohada <91385961+ChiragRohada@users.noreply.github.com>
2022-04-03 23:20:37 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,24 +1,47 @@
 from flask import*
+import os
+
+ 
+app = Flask(__name__)
+ 
+import pyttsx3
 
 @app.route("/")
 def hello_world():
    return render_template('index.html') 
 
+
+
 @app.route("/tts",methods = ['POST', 'GET'])
-def hello_world():
-if request.method == 'POST':
-        text=request.form['text']
-        gender=request.form['custId']
-   
-import pyttsx3
-text_speech = pyttsx3.int()
-result = input("text:")
-text_speech.say(result)
-text_speech.runAndWait()
+def login():
+    if request.method == 'POST':
+      text=request.form['text']
+      gender=request.form['custId']
+      action=request.form['custId2']
+    
+    folder = "C:/Users/webho/Desktop/flask/static/text.mp3"
+    os.remove(folder)
+    
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+    
+    engine.save_to_file(text,'static/text.mp3')
+    engine.runAndWait()
+    engine.stop()
+    return render_template('index.html',text=text)
+    
+ 
+    
+       
+    
+ 
+    
+     
 
-   return render_template('index.html') 
 
-
+    
 
 if __name__ == '__main__':  
    app.run(debug = True)  

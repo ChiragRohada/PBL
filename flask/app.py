b6d05a92c5822ccdea51812a3a73a3c95a6621db
Jainvidhi <95088131+Jainvidhi@users.noreply.github.com>
2022-04-30 09:44:40 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -32,6 +32,26 @@ def login():
     engine.runAndWait()
     engine.stop()
     return render_template('index.html',text=text)
+   
+   import speech_recognition as sr
+   
+   r = sr.Recognizer()
+   
+   while True:
+    with sr.Microphone() as source:
+     r.adjust_for_ambient_noise(source, duration=0.3)
+     print("Listening")
+     
+     audio = r.listen(source)
+     
+     try:
+      text = r.recognize_google(audio)
+      print("Your speech:",text)
+      if text  == 'quit':
+       break
+     except:
+      print("Please say again")
+      
     
  
     
